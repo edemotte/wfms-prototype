66ac589da461b3d0f1dfdd90027157ca8b45a94b
Eugene Demotte <eugenedelain@gmail.com>
2023-04-01 19:50:26 +0530

    added funcitonality to view saved workflows

---
+++ src/components/WorkflowVisualization.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import '../WorkflowVisualization.css';
+
+const WorkflowVisualization = ({ workflow }) => {
+  const renderTasks = () => {
+    return workflow.tasks.map((task, index) => (
+      <div key={index} className="task">
+        {task.name}
+        <div className="task-hover">
+          {task.description && <p>Description: {task.description}</p>}
+          {task.assignee && <p>Assignee: {task.assignee}</p>}
+          {task.dueDate && <p>Due Date: {task.dueDate}</p>}
+          {task.priority && <p>Priority: {task.priority}</p>}
+        </div>
+      </div>
+    ));
+  };
+
+  
+  const renderConnections = () => {
+    const connectors = [];
+  
+    workflow.tasks.forEach((task, taskIndex) => {
+      task.dependencies.forEach((dependencyIndex) => {
+        const startY = 50 * taskIndex + 25;
+        const endY = 50 * dependencyIndex + 25;
+        const middleY = (startY + endY) / 2;
+  
+        connectors.push(
+          <React.Fragment key={`${taskIndex}-${dependencyIndex}`}>
+            <div
+              className="horizontal-line"
+              style={{
+                position: 'absolute',
+                top: `${startY}px`,
+                left: 0,
+                width: '50px',
+                borderBottom: '2px solid #333',
+              }}
+            ></div>
+            <div
+              className="vertical-line"
+              style={{
+                position: 'absolute',
+                top: `${Math.min(startY, endY)}px`,
+                left: 0,
+                height: `${Math.abs(endY - startY)}px`,
+                borderLeft: '2px solid #333',
+              }}
+            ></div>
+            <div
+              className="horizontal-line"
+              style={{
+                position: 'absolute',
+                top: `${endY}px`,
+                left: 0,
+                width: '50px',
+                borderBottom: '2px solid #333',
+              }}
+            ></div>
+          </React.Fragment>
+        );
+      });
+    });
+  
+    return connectors;
+  };
+  
+return (
+  <div className="workflow-visualization">
+    {renderConnections()}
+    {renderTasks()}
+  </div>
+);
+
+};
+
+export default WorkflowVisualization;
